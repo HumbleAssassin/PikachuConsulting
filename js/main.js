7caabf92659c7195398ba462952d69b3f8093b82
Rajeev Thapa <35361381+HumbleAssassin@users.noreply.github.com>
2020-07-22 18:13:01 +0545

    complete (first)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var header = document.querySelector(".navbar-container");
+var goToTop = document.querySelector(".go-to-top")
 
 window.onscroll = function() {
     var top = window.scrollY;
     if (top > 1) {
-        header.classList.add("bg-light");
+        header.classList.add("bg-light-a");
+        goToTop.classList.add("d-block");
     } else {
-        header.classList.remove("bg-light");
+        header.classList.remove("bg-light-a");
+        goToTop.classList.remove("d-block")
     }
 }
